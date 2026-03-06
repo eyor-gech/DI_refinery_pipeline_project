b72138b6692922a0d9f1e65ea2398f31495a80e0
eyor23 <eyor2018@gmail.com>
2026-03-06 15:20:20 +0300

    Intialization

--- src/strategies/__init__.py
+++ src/strategies/__init__.py
@@ -1,5 +1,6 @@
 """Strategy implementations for document extraction."""
 
 from .FastTextExtractor import FastTextExtractor
+from .LayoutExtractor import LayoutExtractor, DoclingDocumentAdapter
 
-__all__ = ["FastTextExtractor"]
+__all__ = ["FastTextExtractor", "LayoutExtractor", "DoclingDocumentAdapter"]
