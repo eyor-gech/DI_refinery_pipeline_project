0683d1f855445273b7ee877f5d261dde00e25b73
eyor23 <eyor2018@gmail.com>
2026-03-06 15:43:12 +0300

    Vision Layout

--- src/strategies/__init__.py
+++ src/strategies/__init__.py
@@ -2,5 +2,6 @@
 
 from .FastTextExtractor import FastTextExtractor
 from .LayoutExtractor import LayoutExtractor, DoclingDocumentAdapter
+from .VisionExtractor import VisionExtractor, CostTracker
 
-__all__ = ["FastTextExtractor", "LayoutExtractor", "DoclingDocumentAdapter"]
+__all__ = ["FastTextExtractor", "LayoutExtractor", "DoclingDocumentAdapter", "VisionExtractor", "CostTracker"]
